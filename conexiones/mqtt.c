513605f16a1bcf32ef52aefd59fa76a0cbcc58bc
jajica <r126401@gmail.com>
2024-08-28 16:46:38 +0200

    de

--- conexiones/mqtt.c
+++ conexiones/mqtt.c
@@ -92,8 +92,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     //esp_mqtt_client_handle_t client = event_data->client;
     int msg_id = -1;
     esp_mqtt_event_handle_t event = event_data;
-    DATOS_APLICACION *datosApp = (DATOS_APLICACION*) event;
-    //datosApp->handle_mqtt = event->data;
+    DATOS_APLICACION *datosApp = (DATOS_APLICACION*) handler_args;
+    datosApp->handle_mqtt = event->data;
 
 
     switch (event->event_id) {
